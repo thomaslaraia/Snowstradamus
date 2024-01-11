2f805c5899697d10262a1abea751400e68a3f110
Thomas Laraia <s1803229@baltic10.geos.ed.ac.uk>
2024-01-11 13:00:50 +0000

    Unfortunately I seem to have successfully established that all the data that I'm failing to download is not from an ROI issue. Maybe I need to ask NASA.

--- scripts/track_pairs.py
+++ scripts/track_pairs.py
@@ -1,13 +1,16 @@
 import os
 
-def track_pairs(dirpath):
+def track_pairs(dirpath, failed = False):
     """
     Goes through each file in a collection of ATL03 and ATL08 files from a given ROI, checks if a file is from ATL03, and if so searches for a corresponding ATL08 file. If it exists, they are appended to a list of filepaths. After collecting all existing file pairs, they are sorted by date.
 
     dirpath - path/to/data/
+    failed - Default False, activate to receive an array of unpaired ATL03 files
     """
     all_ATL03 = []
     all_ATL08 = []
+    
+    failed_ATL03 = []
 
     # Extract characters after 'ATL03' and check for corresponding 'ATL08' file
     for file in os.listdir(dirpath):
@@ -20,8 +23,14 @@ def track_pairs(dirpath):
             if os.path.exists(atl08_file):
                 all_ATL03.append(os.path.join(dirpath, file))
                 all_ATL08.append(atl08_file)
+            else:
+                failed_ATL03.append(os.path.join(dirpath, file))
 
     all_ATL03.sort()
     all_ATL08.sort()
+    failed_ATL03.sort()
 
-    return all_ATL03, all_ATL08
+    if failed == False:
+        return all_ATL03, all_ATL08
+    else:
+        return all_ATL03, all_ATL08, failed_ATL03

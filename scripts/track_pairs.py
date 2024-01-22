ac354c21d10b9c678692deb8b3441364a1cb9daa
Thomas Laraia <s1803229@baltic10.geos.ed.ac.uk>
2024-01-22 15:03:01 +0000

    I'm not sure why, but of the six ATL08 file I looked at, they were doing well with data availability to the north of Sodankyla, but it suddenly disappears. Not sure why. If there is ATL03 data, why isn't there ATL08 data? Is there a way for me to classify myself?

--- scripts/track_pairs.py
+++ scripts/track_pairs.py
@@ -16,12 +16,21 @@ def track_pairs(dirpath, failed = False):
 
     # Extract characters after 'ATL03' and check for corresponding 'ATL08' file
     for file in os.listdir(dirpath):
-        if 'ATL03' in file:
+        if 'processed_ATL03' in file:
             # Extract string of characters after 'ATL03'
             suffix = file.split('ATL03')[1]
 
             # Check if corresponding 'ATL08' file exists
             atl08_file = os.path.join(dirpath, f'processed_ATL08{suffix}')
+            if os.path.exists(atl08_file):
+                all_ATL03.append(os.path.join(dirpath, file))
+                all_ATL08.append(atl08_file)
+            else:
+                failed_ATL03.append(os.path.join(dirpath, file))
+        elif 'ATL03' in file:
+            suffix = file.split('ATL03')[1]
+            
+            atl08_file = os.path.join(dirpath, f'ATL08{suffix}')
             if os.path.exists(atl08_file):
                 all_ATL03.append(os.path.join(dirpath, file))
                 all_ATL08.append(atl08_file)

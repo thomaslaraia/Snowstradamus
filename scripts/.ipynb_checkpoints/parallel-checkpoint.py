b00a9cf6d134b0172ea036e902aaf2e9e7ed5ff2
Thomas Laraia <s1803229@stream.geos.ed.ac.uk>
2024-06-12 12:14:00 +0100

    update images

--- scripts/.ipynb_checkpoints/parallel-checkpoint.py
+++ scripts/.ipynb_checkpoints/parallel-checkpoint.py
@@ -320,7 +320,7 @@ def parallel_odr(dataset, intercepts, maxes, init = -1, lb = -100, ub = -1/100, 
 def pvpg_parallel(atl03path, atl08path, coords, width=4000, height=4000, f_scale = .1, loss = 'arctan', init = -.6, lb = -np.inf, ub = 0,\
     file_index = None, model = parallel_model, res = parallel_residuals, odr = parallel_odr, zeros=None,\
     beam = None, y_init = np.max, graph_detail = 0, canopy_frac = None, terrain_frac = None, keep_flagged=True, opsys='bad', altitude=None,
-                 alt_thresh=200, threshold = 10):
+                 alt_thresh=200, threshold = 2):
     """
     Parallel regression of all tracks on a given overpass.
 

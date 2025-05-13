db1e1078cebefc672225851fdad8b309f111183f
Thomas Laraia <s1803229@may.geos.ed.ac.uk>
2025-05-13 12:30:29 +0100

    rebinned 10m is not as good as 15m. starting to get the landsat val going.

--- scripts/.ipynb_checkpoints/track_pairs-checkpoint.py
+++ scripts/.ipynb_checkpoints/track_pairs-checkpoint.py
@@ -1,6 +1,8 @@
 import os
 import glob
 
+import argparse
+
 def track_pairs(dirpath, failed = False):
     """
     Goes through each file in a collection of ATL03 and ATL08 files from a given ROI, checks if a file is from ATL03, and if so searches for a corresponding ATL08 file. If it exists, they are appended to a list of filepaths. After collecting all existing file pairs, they are sorted by date.
@@ -56,3 +58,20 @@ def track_pairs(dirpath, failed = False):
         return all_ATL03, all_ATL08
     else:
         return all_ATL03, all_ATL08, failed_ATL03
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', type=str, help='directory to look for atl03 and atl08 data')
+    parser.add_argument('--failed', action="store_true", default=False, help='whether to return failed files')
+    args = parser.parse_args()
+
+    #print(args.failed)
+    tracks = track_pairs(args.directory, failed=args.failed)
+    if args.failed == True:
+        print("Failed")
+        for f in tracks[2]:
+            print(f)
+    else:
+        print("Succeeded")
+        for f in tracks[0]:
+            print(f)
